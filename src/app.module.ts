0260cf900efb7c68191499c5af1da6aa31b6fff9
emertola <emertola@gmail.com>
2024-03-27 16:11:19 +0800

    feat: guard task controller

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,16 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { AuthModule } from './auth/auth.module';
 import { TYPE_ORM_CONFIG } from './config/type-orm.config';
 import { PokemonModule } from './pokemon/pokemon.module';
 import { TasksModule } from './tasks/tasks.module';
-import { AuthModule } from './auth/auth.module';
 
 @Module({
-  imports: [TypeOrmModule.forRoot(TYPE_ORM_CONFIG), TasksModule, PokemonModule, AuthModule],
+  imports: [
+    TypeOrmModule.forRoot(TYPE_ORM_CONFIG),
+    TasksModule,
+    PokemonModule,
+    AuthModule,
+  ],
 })
 export class AppModule {}
