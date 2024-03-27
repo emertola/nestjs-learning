bcbfbb9b1b6e1a05bfcfc9d0b9164ec114fdc8b5
emertola <emertola@gmail.com>
2024-03-27 17:17:58 +0800

    fix: temporary db config

--- src/config/type-orm.config.ts
+++ src/config/type-orm.config.ts
@@ -2,13 +2,26 @@ import { TypeOrmModuleOptions } from '@nestjs/typeorm';
 
 export const TYPE_ORM_CONFIG: TypeOrmModuleOptions = {
   type: 'postgres',
-  host: 'localhost',
+  host: 'postgres://taskmanagementuser:rCNPAa8lWD3KoFQ10b9m4QFO8mwJnNo0@dpg-cnm6pknsc6pc739ll040-a/taskmanagement_unv0',
   port: 5432,
-  username: 'postgres',
-  password: 'postgres',
-  database: 'taskmanagement',
+  username: 'taskmanagementuser',
+  password: 'rCNPAa8lWD3KoFQ10b9m4QFO8mwJnNo0',
+  database: 'taskmanagement_unv0',
   entities: [__dirname + '/../**/*.entity.ts'],
   autoLoadEntities: true,
   synchronize: true,
   logging: true,
 };
+
+// export const TYPE_ORM_CONFIG: TypeOrmModuleOptions = {
+//   type: 'postgres',
+//   host: 'localhost',
+//   port: 5432,
+//   username: 'postgres',
+//   password: 'postgres',
+//   database: 'taskmanagement',
+//   entities: [__dirname + '/../**/*.entity.ts'],
+//   autoLoadEntities: true,
+//   synchronize: true,
+//   logging: true,
+// };
