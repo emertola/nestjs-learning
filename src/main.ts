cab7f32f28b7562b4275353b05a2c927c2896252
emertola <emertola@gmail.com>
2024-03-28 20:01:31 +0800

    fix: update env file

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,7 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.setGlobalPrefix('api/v1');
-  console.log('env', process.env.NODE_ENV);
+  console.log('env', process.env.POSTGRESQL_HOST);
   await app.listen(3000);
 }
 bootstrap();
